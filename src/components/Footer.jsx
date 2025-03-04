e1d1873a5727a7bbcb20aa9a37a2152f408abb35
imran <imranhossen0586@gmail.com>
2025-03-05 04:06:00 +0600

    Footer  Done & Navbar TODO

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,15 @@
 import Image from 'next/image';
 import { FaFacebook, FaTwitter, FaLinkedin, FaInstagram } from 'react-icons/fa';
 import logo from '../../public/assets/logo-re.png';
+import Link from 'next/link';
 
 export default function Footer() {
     return (
         <footer className="bg-[#151515] py-12 sm:py-20">
             <div className="container mx-auto px-4">
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-8  justify-items-center lg:justify-items-end">
-                   <div className="text-center lg:text-left">
-                        <Image className='mx-auto lg:m-0' alt="Footer img" width={150} src={logo} />
+                    <div className="text-center lg:text-left">
+                        <Image className='mx-auto lg:m-0' alt="Footer img" width={120} src={logo} />
                         <p className="text-[#E8E8E8] text-sm mb-4">
                             Edwin Diaz is a software and web technologies engineer, a life coach trainer who is also a serial entrepreneur.
                         </p>
@@ -25,10 +26,11 @@ export default function Footer() {
                     <div className="text-center md:text-left">
                         <h3 className="text-white text-xl font-semibold mb-4">About</h3>
                         <ul className="text-[#F3F3F3] space-y-2">
-                            <li><a href="#" className="text-sm">About Us</a></li>
-                            <li><a href="#" className="text-sm">Careers</a></li>
-                            <li><a href="#" className="text-sm">Privacy Policy</a></li>
-                            <li><a href="#" className="text-sm">Terms of Service</a></li>
+                            <li><Link href="/" className="text-sm">Home</Link></li>
+                            <li><Link href="#" className="text-sm">About</Link></li>
+                            <li><Link href="#" className="text-sm">Services</Link></li>
+                            <li><Link href="#" className="text-sm">Products</Link></li>
+                            <li><Link href="#" className="text-sm">Contact</Link></li>
                         </ul>
                     </div>
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,4 @@
 'use client';
-
 import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
@@ -8,7 +7,6 @@ import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
-import Container from "@mui/material/Container";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
@@ -19,7 +17,6 @@ import Image from "next/image";
 import logo from "../../public/assets/logo.svg";
 import Link from "next/link";
 
-const pages = ["Home", "About", "Services", "Blog", "Contact"];
 const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 function NavBar() {
@@ -31,21 +28,66 @@ function NavBar() {
         console.log("Searching for:", searchText);
     };
 
+    const link = (
+        <div className="flex gap-5">
+            <Link
+                className="text-black relative after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-[#FF3811] hover:text-[#FF3811] hover:after:w-full transition-all duration-500 ease-in-out"
+                href="/"
+            >
+                Home
+            </Link>
+            <Link
+                className="text-black relative after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-[#FF3811] hover:text-[#FF3811] hover:after:w-full transition-all duration-500 ease-in-out"
+                href="/about"
+            >
+                About
+            </Link>
+            <Link
+                className="text-black relative after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-[#FF3811] hover:text-[#FF3811] hover:after:w-full transition-all duration-500 ease-in-out"
+                href="/services"
+            >
+                Services
+            </Link>
+            <Link
+                className="text-black relative after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-[#FF3811] hover:text-[#FF3811] hover:after:w-full transition-all duration-500 ease-in-out"
+                href="/products"
+            >
+                Products
+            </Link>
+            <Link
+                className="text-black relative after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-[#FF3811] hover:text-[#FF3811] hover:after:w-full transition-all duration-500 ease-in-out"
+                href="/contact"
+            >
+                Contact
+            </Link>
+        </div>
+
+
+    );
+
     return (
-        <AppBar className="border-b-2 border-b-[#FF3811] py-1  px-0 " style={{ backgroundColor: "white" }} position="fixed">
+        <AppBar
+            className="border-b-2 border-b-[#FF3811] py-3 px-4"
+            style={{ backgroundColor: "white" }}
+            position="fixed"
+        >
             <div className="container mx-auto">
                 <Toolbar disableGutters>
                     {/* Logo for Desktop */}
                     <Box sx={{ display: { xs: "none", md: "flex" }, alignItems: "center", mr: 2 }}>
-                        <Link href={'/'}>
+                        <Link href="/">
                             <Image src={logo} alt="Logo" width={90} height={70} />
                         </Link>
                     </Box>
 
                     {/* Mobile Menu Button */}
                     <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-                        <IconButton size="large" onClick={(e) => setAnchorElNav(e.currentTarget)} color="inherit">
-                            <MenuIcon />
+                        <IconButton
+                            size="large"
+                            onClick={(e) => setAnchorElNav(e.currentTarget)}
+                            color="inherit"
+                        >
+                            <MenuIcon className="text-black" />
                         </IconButton>
                         <Menu
                             id="menu-appbar"
@@ -56,26 +98,42 @@ function NavBar() {
                             open={Boolean(anchorElNav)}
                             onClose={() => setAnchorElNav(null)}
                         >
-                            {pages.map((page) => (
-                                <MenuItem key={page} onClick={() => setAnchorElNav(null)}>
-                                    <Typography sx={{ textAlign: "center" }}>{page}</Typography>
-                                </MenuItem>
-                            ))}
+                            <MenuItem onClick={() => setAnchorElNav(null)}>
+                                <Typography sx={{ textAlign: "center" }}>
+                                    <ul className="px-10">
+                                        <li className="my-2"> <Link className="text-black hover:text-[#FF3811]" href="/">
+                                            Home </Link>
+                                        </li>
+                                        <li className="my-2">
+                                            <Link className="text-black hover:text-[#FF3811]" href="/about">
+                                                About
+                                            </Link>
+                                        </li>
+                                        <li className="my-2">
+                                            <Link className="text-black hover:text-[#FF3811]" href="/services">
+                                                Services
+                                            </Link>
+                                        </li>
+                                        <li className="my-2">
+                                            <Link className="text-black hover:text-[#FF3811]" href="/products">
+                                                Products
+                                            </Link>
+                                        </li>
+                                        <li className="my-2">
+                                            <Link className="text-black hover:text-[#FF3811]" href="/contact">
+                                                Contact
+                                            </Link>
+                                        </li>
+                                    </ul>
+                                </Typography>
+                            </MenuItem>
                         </Menu>
                     </Box>
 
-                    {/* Logo for Mobile */}
-                    <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" }, justifyContent: "center" }}>
-                        <Image src={logo} alt="Logo" width={100} height={50} />
-                    </Box>
 
                     {/* Navbar Links for Desktop */}
                     <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" }, justifyContent: "center" }}>
-                        {pages.map((page) => (
-                            <Button key={page} sx={{ my: 2, color: "black", fontWeight: "700" }}>
-                                {page}
-                            </Button>
-                        ))}
+                        {link}
                     </Box>
 
                     {/* Search and Shopping Icons for Desktop */}
@@ -99,6 +157,11 @@ function NavBar() {
                             padding: "6px 12px",
                             whiteSpace: "nowrap",
                             marginLeft: "20px",
+                            "&:hover": {
+                                backgroundColor: "#FF3811",
+                                color: "white",
+                                borderColor: "#FF3811",
+                            },
                         }}
                         onClick={handleSearch}
                     >
@@ -131,11 +194,13 @@ function NavBar() {
                     </Box>
 
                     <Link href="/login">
-                        <Button>Login</Button>
+                        <Button variant="contained" color="primary" sx={{ marginLeft: "20px" }}>
+                            Login
+                        </Button>
                     </Link>
                 </Toolbar>
-            </div >
-        </AppBar >
+            </div>
+        </AppBar>
     );
 }
 
